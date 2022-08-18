38647e696bdc94aab0a9f7bd136da15332808121
dittrichlucas <ld.dittrich@gmail.com>
2022-08-18 15:35:48 -0300

    test: update user testing steps
    Signed-off-by: dittrichlucas <ld.dittrich@gmail.com>

--- Poc.SpecFlow.ServeRest/Steps/UsersSteps.cs
+++ Poc.SpecFlow.ServeRest/Steps/UsersSteps.cs
@@ -1,31 +1,49 @@
 using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 
 namespace Poc.SpecFlow.ServeRest.Steps;
 
 [Binding]
 public sealed class UsersSteps
 {
-    [Given(@"I access the users route http://localhost:3001/usuarios")]
-    public void GivenIAccessTheUsersRoute()
+    private HttpResponseMessage Response { get; set; } = null!;
+    private static readonly HttpClient Client = new HttpClient();
+
+    [Given(@"I access the users route (.*)")]
+    public async Task GivenIAccessTheUsersRoute(string baseUrl)
     {
-        Console.WriteLine("to implement");
+        var url = new Uri(baseUrl);
+        Response = await Client.GetAsync(url);
     }
 
-    [Then(@"I should have only 1 registered users")]
-    public void ThenIShouldHaveOnlyOneRegisteredUsers()
+    [Then(@"I should have only (.*) registered users")]
+    public async Task ThenIShouldHaveOnlyOneRegisteredUsers(int expect)
     {
-        Console.WriteLine("to implement");
+        var data = await Response.Content.ReadAsStringAsync();
+        var parseData = JObject.Parse(data);
+        var actual = parseData["quantidade"];
+
+        Assert.AreEqual(expect, actual);
     }
 
     [StepDefinition(@"should be admin")]
-    public void AndShouldBeAdmin()
+    public async Task AndShouldBeAdmin()
     {
-        Console.WriteLine("to implement");
+        var data = await Response.Content.ReadAsStringAsync();
+        var parseData = JObject.Parse(data);
+        var actual = parseData["usuarios"][0]["administrador"];
+
+        Assert.AreEqual("true", actual);
     }
 
-    [StepDefinition(@"your name should be Fulano da Silva")]
-    public void AndYourNameShouldBeFulanoDaSilva()
+    [StepDefinition(@"your name should be (.*)")]
+    public async Task AndYourNameShouldBeFulanoDaSilva(string expect)
     {
-        Console.WriteLine("implements");
+        var data = await Response.Content.ReadAsStringAsync();
+        var parseData = JObject.Parse(data);
+        var actual = parseData["usuarios"][0]["nome"]; // TODO: verificar uma forma mais elegante de acessar o valor da prop dentro do objeto (GetValue?)
+
+        Assert.AreEqual(expect, actual);
     }
 }
